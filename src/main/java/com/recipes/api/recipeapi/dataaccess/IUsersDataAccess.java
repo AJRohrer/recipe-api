e6456067fbe790d1fa91980cd70142daf15e1090
Andrew Rohrer <andrewjrohrer@gmail.com>
2019-11-06 07:37:19 -0600

    Finished user get and create in data layer.

--- src/main/java/com/recipes/api/recipeapi/dataaccess/IUsersDataAccess.java
+++ src/main/java/com/recipes/api/recipeapi/dataaccess/IUsersDataAccess.java
@@ -2,12 +2,12 @@ package com.recipes.api.recipeapi.dataaccess;
 
 import com.recipes.api.recipeapi.model.User;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface IUsersDataAccess {
 
     public abstract int createUser(User u);
 
-    public abstract ArrayList<User> getUsers();
+    public abstract List<User> getUsers();
 
 }
--- src/main/java/com/recipes/api/recipeapi/dataaccess/UsersDataAccessImpl.java
+++ src/main/java/com/recipes/api/recipeapi/dataaccess/UsersDataAccessImpl.java
@@ -3,10 +3,21 @@ package com.recipes.api.recipeapi.dataaccess;
 import com.recipes.api.recipeapi.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
 
+import java.awt.geom.GeneralPath;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
+@Repository
 public class UsersDataAccessImpl implements IUsersDataAccess {
 
     @Autowired
@@ -14,11 +25,34 @@ public class UsersDataAccessImpl implements IUsersDataAccess {
 
     @Override
     public int createUser(User u) {
-        return 0;
+        KeyHolder kh = new GeneratedKeyHolder();
+
+        jdbctemplate.update((Connection conn) -> {
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO Users (UserFirstName, UserLastName, UserPassword) VALUES (?,?,?)", Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, u.getUserFirstName());
+            ps.setString(2, u.getUserLastName());
+            ps.setString(3,u.getUserPassword());
+            return ps;
+        }, kh);
+;
+        return kh.getKey().intValue();
     }
 
-    public ArrayList<User> getUsers(){
-        return null;
-    }
+    @Override
+    public List<User> getUsers(){
+        List<User> userList = new ArrayList<>();
+        Collection<Map<String,Object>> rows = jdbctemplate.queryForList("SELECT UserID, UserFirstName, UserLastName, UserPassword FROM Users");
 
+        rows.stream().map((row) -> {
+            User u = new User();
+            u.setUserID(row.get("UserID").toString());
+            u.setUserFirstName(row.get("UserFirstName").toString());
+            u.setUserLastName(row.get("UserLastName").toString());
+            u.setUserPassword(row.get("UserPassword").toString());
+            return u;
+            }).forEach((returnedUser) -> {
+                userList.add(returnedUser);
+            });
+            return userList;
+    }
 }
